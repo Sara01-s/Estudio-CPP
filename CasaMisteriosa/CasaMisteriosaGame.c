3f06a9097dcd7aa7f41791bb4f5c79ae5d474f69
Sara01 <saraosses01@gmail.com>
2022-11-26 19:22:43 -0300

    v1.3.0

--- CasaMisteriosa/CasaMisteriosaGame.c
+++ CasaMisteriosa/CasaMisteriosaGame.c
@@ -54,12 +54,13 @@ void InitObjeto(
 
 
 void InitGameData() {
-    InitHabitacion(&habitaciones[1], 0, 4, 0, 0, "Estas en una pequenia habitacion pintada de blanco. \nJunto a ti puedes ver una cama y un velador con un oso de peluche viejo encima.");
-    InitHabitacion(&habitaciones[2], 0, 5, 0, 0, "Estas en una habitacion pintada de verde. \nJunto a ti puedes ver una cama grande y un velador con fotos de una familia.");
-    InitHabitacion(&habitaciones[3], 0, 6, 0, 0, "Estas en el comedor de la casa. hay una mesa rodeada de sillas.");
+    InitHabitacion(&habitaciones[1], 0, 4, 0, 0, "Estas en una pequenia habitacion pintada de blanco. \nJunto a ti puedes ver una cama y un velador.");
+    InitHabitacion(&habitaciones[2], 0, 5, 0, 0, "Estas en una habitacion pintada de verde oscuro. \nJunto a ti puedes ver una cama grande y un velador con fotos de una familia.");
+    InitHabitacion(&habitaciones[3], 0, 6, 0, 0, "Estas en el comedor de la casa. hay una mesa redonda.");
     InitHabitacion(&habitaciones[4], 1, 7, 5, 0, "Estas en un pasillo, las paredes son blancas y hay poca luz.");
     InitHabitacion(&habitaciones[5], 2, 8, 6, 4, "Estas en un pasillo, las paredes se ven aun mas oscuras.");
-    InitHabitacion(&habitaciones[6], 3, 9, 0, 5, "Estas en un pasillo, ves muchos cuadros y libros.");    InitHabitacion(&habitaciones[7], 4, 0, 0, 0, "Estas en el banio, el lavamanos esta desordenado y el inodoro es cafe.");
+    InitHabitacion(&habitaciones[6], 3, 9, 0, 5, "Estas en un pasillo, ves muchos cuadros y libros.");    
+    InitHabitacion(&habitaciones[7], 4, 0, 0, 0, "Estas en el banio, el lavamanos esta desordenado y el inodoro es cafe.");
     InitHabitacion(&habitaciones[8], 5, 0, 0, 0, "Estas en la entrada de la casa, la puerta se encuentra cerrada.");
     InitHabitacion(&habitaciones[9], 6, 0, 0, 0, "Estas en la cocina, hace frio, es grande y hay mucha luz.");
 
@@ -98,6 +99,7 @@ int main(int argc, char** argv) {
     int i, accion, vueltas;
     int juegoTerminado = 0;
     int indice = 0;
+    int escaleraPuesta = 0;
     
     MostrarComandos();
     
@@ -164,19 +166,19 @@ int main(int argc, char** argv) {
                 
                 accion = 1;
                 TomarObjeto(&objetos[HUESO], 1);
-                printf("\n((Te agachas y recoges el hueso))");
+                printf("\n((Te agachas y recoges el hueso.))");
             }
             
             // Llave
-            if (!strcmp(nombreObjeto, "llave") && objetos[LLAVE].habitacion == habitacionActual) {
+            if (!strcmp(nombreObjeto, "llave") && objetos[LLAVE].habitacion == habitacionActual && objetos[ESCAL].estadoActual == 2) {
 
                 accion = 1;
                 if (objetos[ESCALERA].yaLoTengo) {
                     TomarObjeto(&objetos[LLAVE], 1);
-                    printf("\n((Das la espalda a la puerta y usas la escalera para subir, logras alcanzar la llave y tomarla))");
+                    printf("\n((Das la espalda a la puerta y usas la escalera para subir, logras alcanzar la llave y tomarla.))");
                 }
                 else {
-                    printf("\n((La llaves esta demasiado alta, no puedes alcanzarla))");
+                    printf("\n((La llaves esta demasiado alta, no puedes alcanzarla.))");
                 }
             }
 
@@ -186,15 +188,15 @@ int main(int argc, char** argv) {
                 accion = 1;
                 if (objetos[PERRO].estadoActual == 2) {
                     TomarObjeto(&objetos[ESCALERA], 1);
-                    printf("\n((Te acercas a la escalera, haces fuerza y te la llevas contigo))");
+                    printf("\n((Te acercas a la escalera, haces fuerza y te la llevas contigo.))");
                 }
                 else {
-                    printf("\n((El perro te ladra hostilmente, no te deja acercarte a la escalera))");
+                    printf("\n((El perro te ladra hostilmente, no te deja acercarte a la escalera.))");
                 }
             }
             
             if (accion == 0)
-                printf("\n((No puedes hacer eso))");
+                printf("\n((No puedes hacer eso.))");
         }
 
         if (!strcmp(verbo, "usar")) {
@@ -203,10 +205,11 @@ int main(int argc, char** argv) {
             // Llave
             if (!strcmp(nombreObjeto, "llave") && objetos[LLAVE].yaLoTengo && habitacionActual == 8) {
                 accion = 1;
-                printf("\n((Sientes una brisa en tu espalda, giras la llave y abres la puerta, afuera esta de noche, hay viento y hace frio. Pero haz logrado escapar, felicidades))");
+                printf("\n((Sientes una brisa en tu espalda, giras la llave y abres la puerta, afuera esta de noche, \nhay viento y hace frio. Pero haz logrado escapar, felicidades.))");
+
                 objetos[LLAVE].yaLoTengo = 0;
-                printf("\n\nMuchas gracias por jugar!\n- Juego hecho por Sara01");
-                getchar();
+                printf("\n\nMuchas gracias por jugar!\n- Juego hecho por Sara01.");
+                juegoTerminado = 1;
             }
 
             // Hueso
@@ -216,11 +219,19 @@ int main(int argc, char** argv) {
                 accion = 1;
                 objetos[HUESO].yaLoTengo = 0;
                 objetos[PERRO].estadoActual = 2;
-                printf("\n((El perro se lanza al hueso y se dirige a la esquina a mascarlo))");
+                printf("\n((El perro se lanza al hueso y se dirige a la esquina a mascarlo.))");
+            }
+
+            if (!strcmp(nombreObjeto, "escalera") 
+                && objetos[ESCALERA].yaLoTengo && habitacionActual == 3) {
+
+                accion = 1;
+                objetos[ESCALERA].estadoActual = 2;
+                printf("\n((Pones la escalera en la pared.))");
             }
 
             if (accion == 0)
-                printf("\n((No puedes hacer eso))");
+                printf("\n((No puedes hacer eso.))");
         }
 
         if (!strcmp(verbo, "ir")) {
@@ -247,7 +258,7 @@ int main(int argc, char** argv) {
             }
 
             if (accion == 0)
-                printf("\n((No puedes ir hacia alli))");
+                printf("\n((No puedes ir hacia alli.))");
         }
 
     }
